aee456d0de9e00aabcb4d1ef5773892cd919c816
Jordano Anoia <jordano.anoia@hotmail.ca>
2020-01-27 11:14:52 -0500

    Added Sandbox

---
+++ Sandbox.java
@@ -0,0 +1,11 @@
+public class Sandbox {
+	public static void main(String[] args){
+		System.out.println("Hello Tracos");
+
+		double PI = 3.14159;
+		double radius = 12;
+		double area;
+		area = PI * radius * radius;
+		System.out.println("The area of the circle is" + area);
+	}
+}
